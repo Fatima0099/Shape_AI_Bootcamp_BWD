72d78b6494091323070a1274dc8145942e750e17
Nazreen Fatima <71130516+Fatima0099@users.noreply.github.com>
2021-06-06 20:08:22 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  const curentyear = new Date().getFullYear();
+  return (
+    <div>
+      <footer>
+        <p> Copy @ {curentyear} </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
